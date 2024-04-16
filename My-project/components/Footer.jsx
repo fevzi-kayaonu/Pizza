978ccb5054fc5e858048c7bd07dc281428b70771
fevzi-kayaonu <fevzikayaonu35@gmail.com>
2024-04-16 11:10:11 +0300

    Footer tamamlandı

--- My-project/components/Footer.jsx
+++ My-project/components/Footer.jsx
@@ -5,51 +5,49 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-ust">
-        <div className="footer-container">
-          <div className="address-container">
-            <img
-              className="footer-logo"
-              src="../../Assets/mile2-aseets/footer/logo-footer.svg"
-              alt="logo-footer.png"
-            />
-            <address className="address">
-              <div>
-                <img
-                  src="../../Assets/mile2-aseets/footer/icons/icon-1.png"
-                  alt="Address Icon"
-                />
-                <p>341 Londonderry Road, Istanbul Türkiye</p>
-              </div>
-              <div>
-                <img
-                  src="../../Assets/mile2-aseets/footer/icons/icon-2.png"
-                  alt="Address Icon"
-                />
-                <p>aciktim@teknolojikyemekler.com</p>
-              </div>
-              <div>
-                <img
-                  src="../../Assets/mile2-aseets/footer/icons/icon-3.png"
-                  alt="Address Icon"
-                />
-                <p>+90 216 123 45 67</p>
-              </div>
-            </address>
-          </div>
-          <div className="footer-menuler">
-            <h2>Sıcacık Menuler</h2>
+        <div className="address-container">
+          <img
+            className="footer-logo"
+            src="../../Assets/mile2-aseets/footer/logo-footer.svg"
+            alt="logo-footer.png"
+          />
+          <address className="address">
             <div>
-              <p>Terminal Pizza</p>
-              <p>5 Kişilik Hackathlon Pizza</p>
-              <p>useEffect Tavuklu Pizza</p>
-              <p>Beyaz Console Frosty</p>
-              <p>Testler Geçti Mutlu Burger</p>
-              <p>Position Absolute Acı Burger</p>
+              <img
+                src="../../Assets/mile2-aseets/footer/icons/icon-1.png"
+                alt="Address Icon"
+              />
+              <p>341 Londonderry Road, Istanbul Türkiye</p>
             </div>
+            <div>
+              <img
+                src="../../Assets/mile2-aseets/footer/icons/icon-2.png"
+                alt="Address Icon"
+              />
+              <p>aciktim@teknolojikyemekler.com</p>
+            </div>
+            <div>
+              <img
+                src="../../Assets/mile2-aseets/footer/icons/icon-3.png"
+                alt="Address Icon"
+              />
+              <p>+90 216 123 45 67</p>
+            </div>
+          </address>
+        </div>
+        <div className="footer-menuler">
+          <h4>Sıcacık Menuler</h4>
+          <div>
+            <p>Terminal Pizza</p>
+            <p>5 Kişilik Hackathlon Pizza</p>
+            <p>useEffect Tavuklu Pizza</p>
+            <p>Beyaz Console Frosty</p>
+            <p>Testler Geçti Mutlu Burger</p>
+            <p>Position Absolute Acı Burger</p>
           </div>
         </div>
         <div className="footer-photos">
-          <h2>Instagram</h2>
+          <h4>Instagram</h4>
           <div>
             <img
               src="../../Assets/mile2-aseets/footer/insta/li-0.png"
